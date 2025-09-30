102cd5150b2ceb274a6352f8ad8b48c5a859b9ec
Vincent Huang <vincenth@nvidia.com>
2025-09-30 17:03:34 +0000

    fix regression on SM120
    Signed-off-by: Vincent Huang <vincenth@nvidia.com>

--- cpp/tensorrt_llm/thop/cublasScaledMM.cpp
+++ cpp/tensorrt_llm/thop/cublasScaledMM.cpp
@@ -129,9 +129,9 @@ bool find_special_algo(cublasLtMatmulAlgo_t& algo, std::shared_ptr<CublasMMWrapp
     if ((aType == CUDA_R_16BF || aType == CUDA_R_16F) && (outType == aType || outType == CUDA_R_32F)
         && compType == CUBLAS_COMPUTE_32F)
     {
-        // Spark is compiled with sm120 flag so we check sm120 instead of sm121 here.
-        // TODO: remove this after cublas fix the heuristic for SM120
-        algo_list = tensorrt_llm::common::getSMVersion() == 120 ? spark_bf16_algo_list : bf16_algo_list;
+        // TODO: remove this after cublas fix the heuristic for Spark
+        algo_list = tensorrt_llm::common::getSMVersion(/*queryRealSmArch=*/true) == 121 ? spark_bf16_algo_list
+                                                                                        : bf16_algo_list;
     }
     else if (aType == CUDA_R_8F_E4M3 && compType == CUBLAS_COMPUTE_32F)
     {
